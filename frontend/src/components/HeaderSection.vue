623588fb9cdc9bc1a8a7a7aac107d6711b2dcc37
Fadi <fadih1992@gmail.com>
2025-02-13 19:31:10 -0800

    update

--- frontend/src/components/HeaderSection.vue
+++ frontend/src/components/HeaderSection.vue
@@ -4,16 +4,35 @@
     <nav>
       <ul>
         <li><a href="#">Car Services</a></li>
-        <li><a href="#">Fleet Services</a></li>
+        
+        <div class="dropdown" @click="toggle">
+          <button class="dropdown-btn">Business Programs</button>
+          <ul v-if="isOpen" class="dropdown-menu">
+            <li><a href="#">An item</a></li>
+            <li><a href="#">Another item</a></li>
+            <li><a href="#">Another item</a></li>
+            <li><a href="#">Another item</a></li>
+          </ul>
+        </div>
+
         <li><a href="#">RV services</a></li>
         <li><a href="#">Boats Services</a></li>
         <li><router-link to="/FAQs">FAQs</router-link></li>
         <li><a href="#">Contact</a></li>
       </ul>
+
+
     </nav>
     <a href="#" class="pricing-btn">VIEW OUR PRICING</a>
   </header>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const isOpen = ref(false)
+const toggle = () => isOpen.value = !isOpen.value
+</script>
+
 
 <style scoped>
 .header {
@@ -49,5 +68,24 @@ nav a {
   padding: 10px 15px;
   text-decoration: none;
   border-radius: 5px;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-menu {
+  position: absolute;
+  background: white;
+  border: 1px solid #ccc;
+  list-style: none;
+  padding: 8px 0;
+  margin: 0;
+}
+.dropdown-menu li {
+  padding: 8px 16px;
+}
+.dropdown-menu li:hover {
+  background-color: #eee;
 }
 </style>
