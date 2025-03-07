a0c8fc9adfb8336460b7f23a981e62afbc5081d9
Fadi <fadih1992@gmail.com>
2025-03-07 13:05:29 -0800

    update

--- frontend/src/views/ChoosingServiceView.vue
+++ frontend/src/views/ChoosingServiceView.vue
@@ -8,7 +8,8 @@
         v-for="(service, key) in services"
         :key="key"
         @click="selectService(key)">
-        {{ service.name }}
+        {{ service.name,
+        service.duration }}
       </button>
     </div>
 
