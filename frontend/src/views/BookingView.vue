ece6f24d84d42b5e073a01df9c4192777b5d077c
Fadi <fadih1992@gmail.com>
2025-03-23 22:30:26 -0700

    update

--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -40,8 +40,7 @@ export default {
       address: '',
       message: '',
       bookedTimes: [],
-      timeSlots: ['08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', 
-                  '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM']
+      timeSlots: this.generateTimeSlots("08:00", "17:00", 30)
     };
   },
 
