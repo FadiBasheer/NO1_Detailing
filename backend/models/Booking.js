d0842ea52fd9d22b1eafe1d7d929b3e8be7d26b1
Fadi <fadih1992@gmail.com>
2025-04-06 23:35:03 -0700

    update

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -4,7 +4,7 @@ const bookingSchema = new mongoose.Schema({
   vehicleType: { type: String, required: true },
   date: { type: String, required: true },
   time: { type: String, required: true },
-  address: { type: String, required: true },
+  address: { type: String, required: false },
 });
 
 export default mongoose.model('Booking', bookingSchema);
