c1e2cc38ce3ffdd4a88b9f628f0c25658f2f29c1
Fadi <fadih1992@gmail.com>
2025-06-12 19:45:42 -0700

    update

---
+++ frontend/src/data/vehicles.js
@@ -0,0 +1,10 @@
+// vehicles.js
+export default {
+  Toyota: ["Camry", "Corolla", "RAV4", "Highlander"],
+  Honda: ["Civic", "Accord", "CR-V", "Pilot"],
+  Ford: ["Focus", "Fusion", "Escape", "Explorer"],
+  BMW: ["3 Series", "5 Series", "X3", "X5"],
+  Mercedes: ["C-Class", "E-Class", "GLC", "GLE"],
+  Nissan: ["Altima", "Sentra", "Rogue", "Murano"],
+  Hyundai: ["Elantra", "Sonata", "Tucson", "Santa Fe"],
+};
