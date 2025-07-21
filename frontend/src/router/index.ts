5f19b6d646bdc2343d21de33872a1352e8489960
Fadi <fadih1992@gmail.com>
2025-07-21 10:46:04 -0700

    update

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -30,6 +30,11 @@ const router = createRouter({
        component: () => import('../views/ChoosingServiceView.vue')
     },
     {
+      path: '/thank-you',
+      name: 'thank-you',
+      component: () => import('../views/ThankYouView.vue')
+    },
+    {
       path: '/FAQs',
       name: 'FAQs',
       // route level code-splitting
