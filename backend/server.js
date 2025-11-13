0f885ab817ddc380f6d9ceddc87f0ff9e5ef57e7
Fadi <fadih1992@gmail.com>
2025-11-13 10:00:00 -0800

    Update

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
+const corsOptions = {
   origin: ['http://localhost:5173', 'https://yumeeco.ca'],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  credentials: true
-}));
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
