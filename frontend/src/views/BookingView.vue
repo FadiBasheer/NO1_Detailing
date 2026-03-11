3f85deb095b152e57edfb138f45ae705913e9822
Fadi <fadih1992@gmail.com>
2026-03-11 10:00:00 -0700

    Update

--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -144,6 +144,7 @@
 import axios from "../axios.ts";
 import { services, addons, servicePricing } from "@/data/services.ts";
 import { useAuthStore } from "@/stores/auth.ts";
+import { getClosedMessage } from "@/data/schedule.ts";
 
 export default {
   data() {
