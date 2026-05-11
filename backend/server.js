4c7ebae4e9fe0e443cd3949e9cbc38302a905d6e
Fadi <fadih1992@gmail.com>
2026-05-11 10:00:00 -0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://yumeeco.ca', 'https://www.yumeeco.ca'],
+  origin: ['http://localhost:5173', 'https://yumeeco.ca', 'http://yumeeco.ca', 'https://www.yumeeco.ca', 'http://www.yumeeco.ca'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
